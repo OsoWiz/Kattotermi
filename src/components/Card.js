de518e7a26be4295292a13102a3745bd5c52e9d3
otso <otso.laasonen@aalto.fi>
2023-07-30 16:34:49 +0300

    component files added

---
+++ src/components/Card.js
@@ -0,0 +1,6 @@
+// This component functions as a multipurpose card that can hold text, images, and button functionality in the case of admin.
+
+import React from "react";
+import { useState } from "react";
+
+const Card = (props) => {};
