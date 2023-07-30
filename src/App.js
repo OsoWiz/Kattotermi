b89567c8d944097ae6b73bd3f8d7a2fdd19b2d61
otso <otso.laasonen@aalto.fi>
2023-07-30 15:54:41 +0300

    Jekkui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-function App() {
+import React from "react";
+import { useState } from "react";
+
+function App(props) {
+  const [count, setCount] = useState(0);
+  console.log("App render");
   return (
     <div className="App">
-      <header className="App-header">Kattotermi</header>
+      <header className="App-header">{props.kulli}</header>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
